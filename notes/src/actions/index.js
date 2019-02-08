198f8929df2ed6b8a1ae6c8e968959d3b3d12d23
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-07 21:06:39 -0600

    .get written in actions and reducer

---
+++ notes/src/actions/index.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+export const FETCH_NOTES = 'FETCH_NOTES';
+export const FETCH_NOTES_SUCCESS = 'FETCH_NOTES_SUCCESS';
+export const FETCH_NOTES_FAIL = 'FETCH_NOTES_FAIL';
+
+const URL = 'https://fe-notes.herokuapp.com';
+
+export const fetchNotes = () => dispatch => {
+    dispatch({
+        type: FETCH_NOTES
+    })
+    axios
+        .get(`${URL}/note/get/all`)
+        .then(data => {
+            return dispatch ({
+                type: FETCH_NOTES_SUCCESS,
+                payload: data
+            })
+        })
+        .catch(err => {
+            return dispatch({
+                type: FETCH_NOTES_FAIL,
+                payload: err
+            })
+        })
+}
--- notes/src/reducers/index.js
+++ notes/src/reducers/index.js
@@ -1,12 +1,35 @@
-
+import {
+    FETCH_NOTES,
+    FETCH_NOTES_SUCCESS,
+    FETCH_NOTES_FAIL
+} from '../actions';
 
 
 const initialState = {
-
+    notes: [],
+    fetchingNotes: false,
+    error: null
 }
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
+        case FETCH_NOTES:
+            return {
+                ...state,
+                fetchingNotes: true
+            }
+        case FETCH_NOTES_SUCCESS:
+            return {
+                ...state,
+                fetchingNotes: false,
+                notes: action.payload,
+                error: null
+            }
+        case FETCH_NOTES_FAIL:
+            return {
+                ...state,
+                error: action.payload
+            }
 
         default:
             return state
