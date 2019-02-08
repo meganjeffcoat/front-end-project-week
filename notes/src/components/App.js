e4e401c7c64fc9ae11b2c0c37e5d693f1b3bd3a1
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-07 20:48:55 -0600

    setting up project and file structure

---
+++ notes/src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from 'react';
+import { NavLink, Route } from 'react-router-dom';
+import NotesView from '../views/NotesView';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <nav>
+          <NavLink to='/'>Home</NavLink>
+        </nav>
+        <div className='app-container'>
+          <Route exact path='/' component={NotesView} />
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
