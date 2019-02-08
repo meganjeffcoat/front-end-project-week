c72a9f3e47c790e6aba02c01a137b64da8bbe7b6
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-08 00:03:59 -0600

    action creator for fetchNote created in actions.js

--- notes/src/actions/index.js
+++ notes/src/actions/index.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 export const FETCH_NOTES = 'FETCH_NOTES';
 export const FETCH_NOTES_SUCCESS = 'FETCH_NOTES_SUCCESS';
 export const FETCH_NOTES_FAIL = 'FETCH_NOTES_FAIL';
+export const FETCH_NOTE = 'FETCH_NOTE';
+export const FETCH_NOTE_SUCCESS = 'FETCH_NOTE_SUCCESS';
+export const FETCH_NOTE_FAIL = 'FETCH_NOTE_FAIL';
 
 const URL = 'https://fe-notes.herokuapp.com';
 
@@ -12,15 +15,35 @@ export const fetchNotes = () => dispatch => {
     })
     axios
         .get(`${URL}/note/get/all`)
-        .then(data => {
+        .then(res => {
             return dispatch ({
                 type: FETCH_NOTES_SUCCESS,
-                payload: data.data
+                payload: res.data
             })
         })
         .catch(err => {
             return dispatch({
                 type: FETCH_NOTES_FAIL,
+                payload: err
+            })
+        })
+}
+
+export const fetchNote = id => dispatch => {
+    dispatch({ 
+        type: FETCH_NOTE 
+    })
+    axios
+        .get(`${URL}/note/get/${id}`)
+        .then(res => {
+            return dispatch ({
+                type: FETCH_NOTE_SUCCESS,
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            return dispatch({
+                type: FETCH_NOTE_FAIL,
                 payload: err
             })
         })
